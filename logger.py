39fd54a2622f5450b5d983fe8538ef9fc2d18100
StephenKoulzhik <stephenkoulzhik@gmail.com>
2025-01-19 23:44:49 +0300

    Small changes

--- logger.py
+++ logger.py
@@ -1,8 +1,7 @@
 from aiogram import BaseMiddleware
 from aiogram.types import Message
-import logging
 
 class Logger(BaseMiddleware):
     async def __call__(self, handler, event: Message, data: dict):
-        logging.info(f"Received message: {event.text}")
+        print(f"Получено сообщение: {event.text}")
         return await handler(event, data)
